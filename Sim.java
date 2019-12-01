50fa014716e6a514b52769004cfda019fcf7c06b
GyeongminHa <gaonrudal@gmail.com>
2019-12-01 23:09:13 +0900

    add SIM java, pngs

--- Sim.java
+++ Sim.java
@@ -7,18 +7,29 @@ public class Sim {
 		SimWindow newwindow = new SimWindow(8, 8);
 	}
 }
+
+//로봇이 보고있는 방향
+enum MoveDirection{
+	UP, LEFT, DOWN, RIGHT
+}
+//맵 포인트의 유형
+enum PointType{
+	EMPTY, ROBOT, GOAL, SEENHAZARD, SEENCOLORBLOB, NEWHAZARD, NEWCOLORBLOB
+}
+
 class SimWindow extends JFrame{
 	//프레임 시작위치 설정, 창 크기 설정
-	int framexpos = 100;
-	int frameypos = 100;
-	int framewidth = 1024;
-	int frameheight = 768;
+	private int framexpos = 100;
+	private int frameypos = 100;
+	private int framewidth = 1024;
+	private int frameheight = 768;
 	//맵패널 크기 설정
-	int mapwidth = framewidth/3*2;
-	int mapheight = frameheight;
+	private int mapmargin = 50;
+	private int mapwidth = framewidth/3*2 - mapmargin;
+	private int mapheight = frameheight - mapmargin;
 	//메뉴패널 크기 설정
-	int menuwidth = framewidth/3;
-	int menuheight = frameheight;
+	private int menuwidth = framewidth/3;
+	private int menuheight = frameheight;
 
 	//지도 정보
 	MapData mapdata;
@@ -49,16 +60,21 @@ class SimWindow extends JFrame{
 }
 
 class MapPanel extends JPanel{
-	MapData mapdata;
-	int mapwidth;
-	int mapheight;
-	int cellwidth;
-	int cellheight;
-	int cellimgwidth;
-	int cellimgheight;
+	private MapData mapdata;
+	private int mapwidth;
+	private int mapheight;
+	private int cellwidth;
+	private int cellheight;
+	private int cellimgwidth;
+	private int cellimgheight;
 	
-	private Image originrobotimg;
-	private Image robotimg;
+	private Image imgempty;
+	private Image imgrobot;
+	private Image imggoal;
+	private Image imgseenhazard;
+	private Image imgseencolorblob;
+	private Image imgnewhazard;
+	private Image imgnewcolorblob;
 	//더블버퍼링 위함
 	private Image buffimg; 
 	private Graphics buffimg_g;
@@ -75,8 +91,20 @@ class MapPanel extends JPanel{
 		//MapPanel 크기 초기화
 		setPreferredSize(new Dimension(mapwidth, mapheight));
 		//이미지 로드
-		originrobotimg = Toolkit.getDefaultToolkit().getImage(".\\robotimg.png");
-		robotimg = originrobotimg;//originrobotimg.getScaledInstance(Sim.menuwidth, Sim.menuheight/4 - 30, Image.SCALE_SMOOTH);
+		Image empty = Toolkit.getDefaultToolkit().getImage(".\\EMPTY.png");
+		imgempty = empty.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
+		Image robot = Toolkit.getDefaultToolkit().getImage(".\\ROBOT.png");
+		imgrobot = robot.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
+		Image goal = Toolkit.getDefaultToolkit().getImage(".\\GOAL.png");
+		imggoal = goal.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
+		Image seenhazard = Toolkit.getDefaultToolkit().getImage(".\\SEENHAZARD.png");
+		imgseenhazard = seenhazard.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
+		Image seencolorblob = Toolkit.getDefaultToolkit().getImage(".\\SEENCOLORBLOB.png");
+		imgseencolorblob = seencolorblob.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
+		Image newhazard = Toolkit.getDefaultToolkit().getImage(".\\NEWHAZARD.png");
+		imgnewhazard = newhazard.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
+		Image newcolorblob = Toolkit.getDefaultToolkit().getImage(".\\NEWCOLORBLOB.png");
+		imgnewcolorblob = newcolorblob.getScaledInstance(cellimgwidth, cellimgheight, Image.SCALE_SMOOTH);
 	}
 	
 	@Override
@@ -85,22 +113,50 @@ class MapPanel extends JPanel{
 		buffimg = createImage(mapwidth, mapheight);
 		buffimg_g = buffimg.getGraphics();
 		paintComponents(buffimg_g);
-		buffimg_g.drawImage(robotimg, 0, 0, this);
-		//맵출력
+		//buffimg_g에 맵 출력
 		DrawMap();
 		//버퍼링 마친 이미지 한번에 출력
 		g.drawImage(buffimg, 0, 0, null);
 	}
 	
 	public void DrawMap() {
-		int cell_x;
-		int cell_y;
+		int mapmargin = 10;
+		int xpos, ypos;
 		for(int i = 0; i < mapdata.maprow; i++) {
 			for(int j = 0; j < mapdata.mapcol; j++) {
-				cell_x = i*cellwidth;
-				cell_y = j*cellheight;
-				buffimg_g.drawRect(cell_x, cell_y, cellwidth, cellheight);
+				xpos = i*cellwidth + mapmargin;
+				ypos = j*cellheight + mapmargin;
+				DrawPoint(i,j, xpos, ypos);
 			}
+		}
+	}
+	
+	public void DrawPoint(int row, int col, int xpos, int ypos) {
+		switch(mapdata.mapMatrix[row][col]) {
+		case EMPTY:
+			buffimg_g.drawImage(imgempty, xpos, ypos, this);
+			break;
+		case ROBOT:
+			buffimg_g.drawImage(imgrobot, xpos, ypos, this);
+			break;
+		case GOAL:
+			buffimg_g.drawImage(imggoal, xpos, ypos, this);
+			break;
+		case SEENHAZARD:
+			buffimg_g.drawImage(imgseenhazard, xpos, ypos, this);
+			break;
+		case SEENCOLORBLOB:
+			buffimg_g.drawImage(imgseencolorblob, xpos, ypos, this);
+			break;
+		case NEWHAZARD:
+			buffimg_g.drawImage(imgnewhazard, xpos, ypos, this);
+			break;
+		case NEWCOLORBLOB:
+			buffimg_g.drawImage(imgnewcolorblob, xpos, ypos, this);
+			break;
+		default:
+			System.out.println("error in DrawPoint");
+			break;
 		}
 	}
 }
@@ -168,60 +224,58 @@ class smallImgPanel extends JPanel{
 }
 
 class MapData{
-	enum moveDirection{ //로봇이 보고있는 방향
-		UP, LEFT, DOWN, RIGHT
-	}
-	enum pointType{ //맵에 나타낼 개별 점들
-		EMPTY, ROBOT, START, GOAL, HAZARD, NEWHAZARD, SEENHAZARD, NEWCOLORBLOB, SEENCOLORBLOB
-	}
 	String robotpath; //path 명령줄
 	
 	int maprow;
 	int mapcol;
-    pointType[][] mapMatrix; //맵 정보
+    PointType[][] mapMatrix; //맵 정보
 	
     int[] truePosition = new int[2]; //실제 로봇 위치
 	int[] currentPosition = new int[2]; //예측된 현재 로봇 위치
-	moveDirection currentDirection = moveDirection.UP; //현재 로봇 방향
+	MoveDirection currentDirection = MoveDirection.UP; //현재 로봇 방향
 
 	public MapData(int row, int col) {
 		maprow = row;
 		mapcol = col;
 		
-		mapMatrix = new pointType[maprow][mapcol];
+		mapMatrix = new PointType[maprow][mapcol];
 		//모든 칸을 EMPTY로 초기화
 		for(int i = 0; i < maprow; i++) {
 			for(int j = 0; j < mapcol; j++) {
-				mapMatrix[i][j] = pointType.EMPTY;
+				mapMatrix[i][j] = PointType.EMPTY;
 			}
 		}
+		
+		mapMatrix[0][0] = PointType.ROBOT;
+		mapMatrix[0][1] = PointType.SEENHAZARD;
+		mapMatrix[0][2] = PointType.SEENCOLORBLOB;
+		mapMatrix[0][3] = PointType.NEWHAZARD;
+		mapMatrix[0][4] = PointType.NEWCOLORBLOB;
+		mapMatrix[0][5] = PointType.GOAL;
 	}
 	
-    public void setMapPoint(pointType type, int row, int col) {
+    public void setMapPoint(PointType type, int row, int col) {
 		switch(type) { //EMPTY만 없음
 		case ROBOT:
-			mapMatrix[row][col] = pointType.ROBOT;
-			break;
-		case START:
-			mapMatrix[row][col] = pointType.START;
+			mapMatrix[row][col] = PointType.ROBOT;
 			break;
 		case GOAL:
-			mapMatrix[row][col] = pointType.GOAL;
-			break;
-		case HAZARD:
-			mapMatrix[row][col] = pointType.HAZARD;
-			break;
-		case NEWHAZARD:
-			mapMatrix[row][col] = pointType.NEWHAZARD;
+			mapMatrix[row][col] = PointType.GOAL;
 			break;
 		case SEENHAZARD:
-			mapMatrix[row][col] = pointType.SEENHAZARD;
-			break;
-		case NEWCOLORBLOB:
-			mapMatrix[row][col] = pointType.NEWCOLORBLOB;
+			mapMatrix[row][col] = PointType.SEENHAZARD;
 			break;
 		case SEENCOLORBLOB:
-			mapMatrix[row][col] = pointType.SEENCOLORBLOB;
+			mapMatrix[row][col] = PointType.SEENCOLORBLOB;
+			break;
+		case NEWHAZARD:
+			mapMatrix[row][col] = PointType.NEWHAZARD;
+			break;
+		case NEWCOLORBLOB:
+			mapMatrix[row][col] = PointType.NEWCOLORBLOB;
+			break;
+		default:
+			System.out.println("error in setMapPoint");
 			break;
 		}
 	}
